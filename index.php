bf05ca9ef3a612118fe7304f066f0d6518b876d1
ada2003 <salveadarsh23@gmail.com>
2026-09-03 23:22:37 +0530

    LAst chds

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A Little Man - Navbar</title>
+    <title>A Little Man </title>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="style.css">
